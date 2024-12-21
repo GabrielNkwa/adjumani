ac35246e8b0d28526d7247f0174b01bf8e59935e
Gabriel Nkwa <gnkwa@outlook.com>
2024-12-21 22:07:21 +0100

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,16 +4,37 @@ import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-blue-600 p-4">
+    <nav className="bg-blue-600 p-4 fixed top-0 left-0 w-full z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-white text-2xl font-bold">ADJUMANI</Link>
+        <Link to="/" className="text-white text-2xl font-bold">
+          ADJUMANI
+        </Link>
         <ul className="flex space-x-4">
-          <li><Link to="/" className="text-white hover:underline">Home</Link></li>
-          <li><Link to="/about" className="text-white hover:underline">About</Link></li>
-          <li><Link to="/works" className="text-white hover:underline">Our Works</Link></li>
-          <li><Link to="/involved" className="text-white hover:underline">Get Involved</Link></li>
-          <li><Link to="/contact" className="text-white hover:underline">Contact</Link></li>
-          
+          <li>
+            <Link to="/" className="text-white hover:underline">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="text-white hover:underline">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/works" className="text-white hover:underline">
+              Our Works
+            </Link>
+          </li>
+          <li>
+            <Link to="/involved" className="text-white hover:underline">
+              Get Involved
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="text-white hover:underline">
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
